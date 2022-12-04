5cc5c67b2d14d34ac198410739e2d82644c28378
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-12-04 08:22:34 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# try:
+#     file= open("a_file.txt")
+#     a_dict= {"key": "value"}
+#     print(a_dict["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("qualquer coisa")
+# except KeyError as error_message:
+#     print(f"A chave {error_message} não existe.")
+# else:
+#     content = file.read()
+#     print(content)
+
+
+# "crl + k + c"
