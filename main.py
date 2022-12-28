2ca7a5cad6d7f564d2656824353a4be3439ef4d0
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-12-28 09:35:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,14 +40,15 @@ def save():
         messagebox.showinfo(title="Oops", message="Por favor, certifique-se de que não deixou nenhum campo em branco.") #para apresentar uma menssagem caso tenha campos em branco e não deixe salvar.
     
     else:    
-        with open("data.json", "r") as data_file:
+        with open("data.json", "r") as data_file: #"r" modo de leitura
             
             #LENDO OS DADOS GRAVADOS:
             data = json.load(data_file)  #o modo .load é modo de leitura do ficheiro. Utilizamos para chamar os dados do arquivo json e utlizarmos como um dicioanrio python.
            
             #ADICIONANDO NOS DADOS GRAVADOS OS NOVOS DADOS:
             data.update(new_dicio) #o modo json.update é como o append
-           
+        
+        with open("data.json", "w") as data_file: #"w" modo de escrita.
             #GRAVANDO OS DADOS ATUALIZADOS:
             json.dump(data, data_file, indent= 4)  #modo .dump é utilizado para escrever no arquivo. ex json.dump(onde/como?, e quais dados?, indent=numero de linhas? para melhor visualizar)
             
